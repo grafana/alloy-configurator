b6ae663bbcd2a55ed5da47ea1ce7d814020c6430
Dominik Süß <dominik@suess.wtf>
2024-05-02 08:08:04 +0200

    fix: jest testcases with grafana ui components

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+Object.defineProperty(global, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
